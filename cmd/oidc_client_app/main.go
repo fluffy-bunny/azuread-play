ec2d08c2aa2d875fe122f4eeab1c62da18a75295
Herb Stahl <ghstahl@gmail.com>
2023-10-04 11:34:00 -0700

    a full login client that does a followup by pulling groups

---
+++ cmd/oidc_client_app/main.go
@@ -0,0 +1,158 @@
+/*
+This is an example application to demonstrate querying the user info endpoint.
+*/
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"azuread-play/internal/azureclient"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+)
+
+func randString(nByte int) (string, error) {
+	b := make([]byte, nByte)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(b), nil
+}
+
+func setCallbackCookie(w http.ResponseWriter, r *http.Request, name, value string) {
+	c := &http.Cookie{
+		Name:     name,
+		Value:    value,
+		MaxAge:   int(time.Hour.Seconds()),
+		Secure:   r.TLS != nil,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, c)
+}
+
+const (
+	port              = ":5556"
+	domain            = "localhost"
+	discoveryBaseURLT = "https://login.microsoftonline.com/%s/v2.0"
+	issuerUrlT        = "https://login.microsoftonline.com/%s/v2.0"
+)
+
+var (
+	fullDomain = domain + port
+)
+
+func main() {
+	ctx := context.Background()
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	// create a logger and add it to the context
+	log := zerolog.New(os.Stdout).With().Caller().Timestamp().Logger()
+	ctx = log.WithContext(ctx)
+
+	err := godotenv.Overload()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error loading .env file")
+	}
+	tenantID := os.Getenv("AZURE_TENANT_ID")
+	clientID := os.Getenv("AZURE_CLIENT_ID")
+	clientSecret := os.Getenv("AZURE_CLIENT_SECRET")
+
+	client, err := azureclient.NewClient(tenantID, clientID, clientSecret)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error creating client")
+	}
+
+	discoveryBaseURL := fmt.Sprintf(discoveryBaseURLT, tenantID)
+	issuerUrl := fmt.Sprintf(issuerUrlT, tenantID)
+
+	ctx = oidc.InsecureIssuerURLContext(ctx, issuerUrl)
+
+	provider, err := oidc.NewProvider(ctx, discoveryBaseURL)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to query provider")
+	}
+	endpoint := provider.Endpoint()
+	config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     endpoint,
+		RedirectURL:  "http://localhost:5556/auth/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "User.Read"},
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		state, err := randString(16)
+		if err != nil {
+			http.Error(w, "Internal error", http.StatusInternalServerError)
+			return
+		}
+		setCallbackCookie(w, r, "state", state)
+
+		http.Redirect(w, r, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	http.HandleFunc("/auth/callback", func(w http.ResponseWriter, r *http.Request) {
+		state, err := r.Cookie("state")
+		if err != nil {
+			http.Error(w, "state not found", http.StatusBadRequest)
+			return
+		}
+		if r.URL.Query().Get("state") != state.Value {
+			http.Error(w, "state did not match", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		userInfo, err := provider.UserInfo(ctx, oauth2.StaticTokenSource(oauth2Token))
+		if err != nil {
+			http.Error(w, "Failed to get userinfo: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		groups := []string{}
+		user, err := client.GetUserByEmail(ctx, userInfo.Email)
+		if err != nil {
+			log.Error().Err(err).Msg("Error getting user")
+		} else {
+			client.IterateUserGroups(ctx, user.ID, func(group string) bool {
+				groups = append(groups, group)
+				return true
+			})
+		}
+
+		resp := struct {
+			OAuth2Token *oauth2.Token
+			UserInfo    *oidc.UserInfo
+			Groups      []string
+		}{oauth2Token, userInfo, groups}
+
+		data, err := json.MarshalIndent(resp, "", "    ")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(data)
+	})
+
+	log.Printf("listening on http://%s/", fullDomain)
+	err = http.ListenAndServe(fullDomain, nil)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error listening")
+	}
+
+}
--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.21.1
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.3.1
+	github.com/coreos/go-oidc/v3 v3.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/microsoft/kiota-abstractions-go v1.2.2
 	github.com/microsoft/kiota-serialization-json-go v1.0.4
 	github.com/microsoftgraph/msgraph-sdk-go v1.19.0
 	github.com/microsoftgraph/msgraph-sdk-go-core v1.0.0
 	github.com/rs/zerolog v1.31.0
+	golang.org/x/net v0.15.0
+	golang.org/x/oauth2 v0.12.0
 )
 
 require (
@@ -18,9 +21,11 @@ require (
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.1.1 // indirect
 	github.com/cjlapao/common-go v0.0.39 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.0 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -37,9 +42,10 @@ require (
 	go.opentelemetry.io/otel v1.18.0 // indirect
 	go.opentelemetry.io/otel/metric v1.18.0 // indirect
 	go.opentelemetry.io/otel/trace v1.18.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
-	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
